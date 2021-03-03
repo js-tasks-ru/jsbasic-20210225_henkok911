81e8be17d1861f51eb9235015bc2ae12be81a310
Anton Demidov <gkukishek@gmail.com>
2021-03-03 22:32:57 +0200

    задача факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  let total = 1;
+  if (n!=0 || n!=1){
+    for(let i=0;i<n;i++){
+    
+      total = total*(n-i);
+    }
+    return total;
+  } return total;
 }
