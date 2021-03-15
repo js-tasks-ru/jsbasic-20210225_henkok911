ee511f6d251e80bd8f30aef5c5d16ee257b40e41
Anton Demidov <gkukishek@gmail.com>
2021-03-15 22:32:02 +0200

    переделаная задача факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,10 +1,13 @@
 function factorial(n) {
+  
+  if (n == 0 || n == 1){
+    return 1;
+  } 
   let total = 1;
-  if (n!=0 || n!=1){
-    for(let i=0;i<n;i++){
+  for(let i=0;i<n;i++){
     
-      total = total*(n-i);
-    }
-    return total;
-  } return total;
+    total = total*(n-i);
+  }
+  
+  return total;
 }
