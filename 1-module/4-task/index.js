02cca41f97ed1788748752ca021634c03e4a9c0f
Anton Demidov <gkukishek@gmail.com>
2021-03-25 07:24:35 +0200

    задача 4 модуль 1 version 2

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,4 +1,4 @@
 function checkSpam(str) {
   let lowStr = str.toLowerCase();
-  return lowStr.includes('1xbet') || lowStr.includes('xxx') ? true : false;
+  return (lowStr.includes('1xbet') || lowStr.includes('xxx'));
 }
