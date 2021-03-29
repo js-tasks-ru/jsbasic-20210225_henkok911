f918087060c5e0e122e2c4e16f9ce654fd84994a
Anton Demidov <gkukishek@gmail.com>
2021-03-29 22:16:45 +0300

    задача 5 модуль 1 + задача 1-2-3 модуль 2 (version 2)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,12 +1,6 @@
 function sumSalary(salaries) {
- /*  let result = 0;
-  for (let i in salaries) {
-    typeof salaries[i] !== 'number' ? result : result += salaries[i]
-  } 
-  return result */
-
-  return Object.values(salaries).reduce((acc,i) => {
-    typeof i === 'number' && isFinite(i) && !isNaN(i) ? acc += i : acc
-    return acc
+  return Object.values(salaries).reduce((total,money) => {
+    typeof money === 'number' && isFinite(money) ? total += money : total
+    return total
   },0)
 }
